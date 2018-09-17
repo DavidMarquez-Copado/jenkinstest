e27d4635135ed309c7b7ba061f86ad66d27fdb80
David Marquez Delgado <dmarquez@macbook-air-copado.local>
2018-09-17 08:25:08 +0200

    Changed java file

--- src/main/java/org/jacoco/examples/maven/java/HelloWorld2.java
+++ src/main/java/org/jacoco/examples/maven/java/HelloWorld2.java
@@ -2,7 +2,6 @@ package org.jacoco.examples.maven.java;
 
 public class HelloWorld2 {
 	//NEW COMMENT
-	// Added other comment
 	public String getMessage(boolean bigger) {
 		if (bigger) {
 			return "Hello Universe!";
